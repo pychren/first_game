7efa191f38f58bbb0a02bc785247aaadd9a42d16
username <username@example.com>
2025-05-05 18:16:52 +0800

    0.2.3

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -6,8 +6,11 @@ extends Area2D
 
 func _on_body_entered(_body: Node2D) -> void:
 	print("You died!")
+	Engine.time_scale = 0.5
+	_body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+	Engine.time_scale = 1.0
